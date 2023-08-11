fa64d35ca6477e9c982b42e6b3bfdcd5eb7c7063
jusrus01 <jjustinas.ruslys@gmail.com>
2023-08-11 17:52:12 +0300

    fix markdown issue

--- src/app/modules/post/post.component.ts
+++ src/app/modules/post/post.component.ts
@@ -3,25 +3,23 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PostStore } from './post.store';
 
-import 'prismjs';
-import 'prismjs/components/prism-typescript.min.js';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
-import 'prismjs/plugins/line-highlight/prism-line-highlight.js';
-
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
 })
 export class PostComponent {
-  public readonly vm$: Observable<{postBody: string | null, isLoading: boolean}>;
+  public readonly vm$: Observable<{
+    postBody: string | null;
+    isLoading: boolean;
+  }>;
 
   constructor(private store: PostStore, private route: ActivatedRoute) {
     this.vm$ = store.vm$;
   }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(paramMap => {
+    this.route.paramMap.subscribe((paramMap) => {
       const fileName = paramMap.get('fileName');
       if (!fileName) {
         return;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // https://tailwindcss.com/docs/preflight#headings-are-unstyled
+  // https://tailwindcss.com/docs/upgrade-guide#base-layer-must-be-present
+  corePlugins: {
+    preflight: false
+  },
   content: [
     "./src/**/*.{html,ts}",
   ],
